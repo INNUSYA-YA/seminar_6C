e37bf28ebe9ef9f4c40d9f34f62b793a11028f85
Inna <innasilivonchik@mail.ru>
2022-07-15 22:00:27 +0300

    Задача 41

---
+++ Task_41/Program.cs
@@ -0,0 +1,15 @@
+﻿//  Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+Console.Write("ВВедите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("ВВедите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("ВВедите третье число ");
+int c = Convert.ToInt32(Console.ReadLine());
+if(a+b>c&&a+c>b&&b+c>a)
+{
+    Console.WriteLine("треугольник сo сторонами такой длины может существовать");
+}
+else
+{
+    Console.WriteLine("треугольник сo сторонами такой длины  не может существовать");
+}
---
+++ Task_42/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+Console.Write("ВВедите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+string b = "";
+
+while (a>0)
+{
+    b = a%2+b;
+    a/=2;
+}
+Console.WriteLine(b);
