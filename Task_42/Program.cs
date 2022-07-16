593cb088e8bd1ec6414d58c4887ca3cbc89edb6e
Inna <innasilivonchik@mail.ru>
2022-07-16 07:32:21 +0300

    Задача 42

--- Task_42/Program.cs
+++ Task_42/Program.cs
@@ -1,15 +1,11 @@
 ﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
 Console.Write("ВВедите число ");
 int number = Convert.ToInt32(Console.ReadLine());
+string a = "";
 
-ToBin(number);
-
-void ToBin(int number)
+while(number>0)
 {
-    if (number == 0)
-    {
-        return;
-    }
-    ToBin(number/2);
-    Console.Write(number%2);  
+    a = number%2+a;
+    number/=2;
 }
+Console.WriteLine(a);
